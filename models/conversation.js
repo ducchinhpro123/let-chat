3d3b8ee7871777fdfce9898f2fd98d4cfc5c0921
djksanrs <chinh99562@donga.edu.vn>
2024-12-24 11:29:44 +0700

    Add more field for conversation schema

--- models/conversation.js
+++ models/conversation.js
@@ -2,11 +2,12 @@ import mongoose, { Schema } from "mongoose";
 
 const conversationSchema = new Schema({
   type: { type: String, enum: ['public', 'private'], required: true},
+  name: { type: String, required: true },
   participants: [{
     user: { type: Schema.Types.ObjectId, ref: 'User' },
     role: { type: String, enum: ['member', 'admin'], default: 'member' },
   }],
-  lastMessage: { type: Schema.Types.ObjectId, ref: 'Message' },
+  latestMessage: { type: Schema.Types.ObjectId, ref: 'Message' },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now }
 
